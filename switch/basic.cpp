51913082294ca8de8daca750bc7421972d5db8b9
Shibli <ahmedrazashibli@gmail.com>
2023-07-21 21:02:56 +0530

    Calc in cpp

--- switch/basic.cpp
+++ switch/basic.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main(){
+    int num = 2;
+    char ch = 'a';
+
+    cout << endl;
+
+    switch (ch)
+    // switch (num)
+    {
+    case 1: cout << "First" << endl;
+        break;
     
+    case 2: cout << "Second" << endl;
+        break;
+
+    case 'a': cout << "Charachter" << endl;
+        break;
+    
+    default: cout << "It is a default case" << endl;
+    }
 }
