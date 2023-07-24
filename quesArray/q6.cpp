073c5d1858faa5d502e95f56e201f1a06192ecf4
Shibli <ahmedrazashibli@gmail.com>
2023-07-24 20:51:52 +0530

    Questions

--- quesArray/q6.cpp
+++ quesArray/q6.cpp
@@ -12,17 +12,23 @@ vector<int> intersection(int arr1[], int arr2[], int size){
 
         for (int j = 0; j <= size; j++)
         {
+            if(element < arr2[j]){
+                break;
+            }
             if(element == arr2[j]){
                 ans.push_back(element);
                 arr2[j] = -112233;
                 break;
             }
         }
-        
     }
     return ans;
 }
 
 int main(){
-
+    int arr1[5] = {1,2,3,4,5};
+    int arr2[2] = {2,5};
+    int size;
+    cin >> size;
+    // int ans = intersection(arr1, arr2, size);
 }
---
+++ quesArray/q7.cpp
@@ -0,0 +1,12 @@
+// Pair sum leet code
+
+#include <iostream>
+using namespace std;
+
+int pairSum(){
+
+}
+
+int main(){
+
+}
