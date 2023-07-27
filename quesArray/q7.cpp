1e7e78c42c70caaa8cbe2a506fe49b915f4c7f70
Shibli <ahmedrazashibli@gmail.com>
2023-07-27 21:59:07 +0530

    Pair sum done

--- quesArray/q7.cpp
+++ quesArray/q7.cpp
@@ -7,24 +7,22 @@ int pairSum(int arr[], int size, int ans){
     int temp = 0;
     for (int i = 0; i < size; i++)
     {
-        temp = temp + arr[i];
-        if(temp<ans){
-            continue;
-        }
-        else if(temp = ans){
-            break;
-        }
-        else{
-            break;
-        }
+        for (int j = i+1; j < size; j++)
+        {
+            if(arr[i]+arr[j]==ans){
+                temp = temp + arr[i] + arr[j];
+                return temp;
+                
+            }
+        } 
     }
-    return temp;
+    return -1;
     
 
 }
 
 int main(){
     int ansToBe=23;
-    int arr[10] = {1,2,4,5,82,13,54,10,9,0};
+    int arr[10] = {11,1,4,5,82,13,54,10,9,0};
     cout << pairSum(arr, 10, ansToBe);
 }
